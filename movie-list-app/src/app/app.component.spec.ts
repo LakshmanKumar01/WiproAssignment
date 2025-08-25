67ff7cc9caf29521b68166b0007bcc8684b23fbc
LakshmanKumar01 <gollalakshmankumar2003@gmail.com>
2025-08-25 15:27:16 -0400

    Feedback-Form assignment is added

--- movie-list-app/src/app/app.component.spec.ts
+++ movie-list-app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'movie-list-app' title`, () => {
+  it(`should have the 'feedback-form-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('movie-list-app');
+    expect(app.title).toEqual('feedback-form-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, movie-list-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, feedback-form-app');
   });
 });
